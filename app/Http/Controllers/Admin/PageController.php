03f02c1831c0f4bb340316f0980e4e55ab9616f1
vilpilman <vilpilman@yahoo.com>
2020-02-29 00:14:40 +0200

    Admin PageController created

--- app/Http/Controllers/Admin/PageController.php
+++ app/Http/Controllers/Admin/PageController.php
@@ -4,82 +4,59 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Page;
+use App\Jobs\UploadPagePhoto;
 
 class PageController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
-        //
+        $pages = Page::all();
+        $data = [
+            'pages' => $pages,
+        ];
+        return view('admin.pages.index', $data);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
-        //
+        return view('admin.pages.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        $page = Page::create($request->all());
+        return redirect()
+                    ->route('pages.index')
+                    ->with('success', "Created successfuly");
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    public function edit(Page $page)
     {
-        //
+        $data = [
+            'page' => $page,
+        ];
+        return view('admin.pages.edit', $data);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
+    public function update(Request $request, Page $page)
     {
-        //
+        $page->update($request->all());
+
+        if (isset($request->photo))
+            UploadPagePhoto::dispatchNow($page, $request->file('photo'));
+
+        return redirect()
+                    ->route('pages.index')
+                    ->with('success', "Updated successfuly");
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+    public function destroy(Page $page)
     {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        $page->delete();
+        return redirect()
+                    ->back()
+                    ->with('success', "Deleted successfuly");
     }
 }
