ab9ed192d7781e1213735db16e64c7aeac0eb5d3
vilpilman <vilpilman@yahoo.com>
2020-02-29 00:15:04 +0200

    page resource routes created

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware('auth')->resource('/admin/pages', 'admin\PageController');
