4668ff7973291df75e00199385f96225bb5cf9a4
vilpilman <vilpilman@yahoo.com>
2020-02-29 01:07:24 +0200

    clean code

--- resources/views/admin/pages/create.blade.php
+++ resources/views/admin/pages/create.blade.php
@@ -1,23 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Create Page</div>
-                <div class="card-body">
-                    @include('admin.includes.alerts.show_success')
-                    @include('admin.includes.alerts.show_errors') 
-                    
-                    {{ Form::open(['files'=>true, 'route' => 'pages.store', 'method'=>'POST']) }}
-                        <div class="row">
-                            @include('admin.pages.fields')
-                        </div>   
-                    {{ Form::close() }} 
-                </div>
-            </div>
+    <div class="card">
+        <div class="card-header">Create Page</div>
+        <div class="card-body">
+            @include('admin.includes.alerts.show_success')
+            @include('admin.includes.alerts.show_errors') 
+            
+            {{ Form::open(['files'=>true, 'route' => 'pages.store', 'method'=>'POST']) }}
+                <div class="row">
+                    @include('admin.pages.fields')
+                </div>   
+            {{ Form::close() }} 
         </div>
     </div>
-</div>
 @endsection
