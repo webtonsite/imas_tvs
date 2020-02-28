e3f15e0030e6c3f6ae5ba8a1fb195f62d0d2781e
vilpilman <vilpilman@yahoo.com>
2020-02-29 01:06:16 +0200

    admin menu created

---
+++ resources/views/admin/includes/admin_menu.blade.php
@@ -0,0 +1,11 @@
+<ul class="admin-menu">
+    <li>
+        <a href="{{ route('admin') }}">Home</a>
+    </li>
+    <li>
+        <a href="{{ route('pages.index') }}">Pages</a>
+    </li>
+    <li>
+        <a href="{{ route('pages.create') }}">New Page</a>
+    </li>
+</ul>
