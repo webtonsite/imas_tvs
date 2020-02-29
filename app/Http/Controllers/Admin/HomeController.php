1eee04df4eada4f4813a564c3bf619d2409bdeaf
vilpilman <vilpilman@yahoo.com>
2020-02-29 05:21:16 +0200

    HomeController updated - shows Exchange Rates (EUR, USD, GBP, RUB)

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -4,9 +4,15 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Services\ExchangeRatesService;
 
 class HomeController extends Controller
 {
+    private $eur_data;
+    private $usd_data;
+    private $gbp_data;
+    private $rub_data;
+
     /**
      * Create a new controller instance.
      *
@@ -14,7 +20,23 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        //
+        $exchange_rates = new ExchangeRatesService();
+
+        $res = $exchange_rates->get('EUR');
+        if($res->getStatusCode() === 200)
+            $this->eur_data = json_decode( $res->getBody() );
+        
+        $res = $exchange_rates->get('USD');
+        if($res->getStatusCode() === 200)
+            $this->usd_data = json_decode( $res->getBody() );
+
+        $res = $exchange_rates->get('GBP');
+        if($res->getStatusCode() === 200)
+            $this->gbp_data = json_decode( $res->getBody() );
+
+        $res = $exchange_rates->get('RUB');
+        if($res->getStatusCode() === 200)
+            $this->rub_data = json_decode( $res->getBody() );
     }
 
     /**
@@ -24,6 +46,13 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('admin.home');
+        $data = [
+            'eur_data' => $this->eur_data,
+            'usd_data' => $this->usd_data,
+            'gbp_data' => $this->gbp_data,
+            'rub_data' => $this->rub_data,
+        ];
+        
+        return view('admin.home', $data);
     }
 }
